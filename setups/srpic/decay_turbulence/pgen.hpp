c404c7825d375ae9aac7f3e5467d51f55e7de994
StaticObserver <cyysouldancer@gmail.com>
2024-11-13 13:50:02 +0800

    update decay_turb to be compatible with all 3 dimensions

--- setups/srpic/decay_turbulence/pgen.hpp
+++ setups/srpic/decay_turbulence/pgen.hpp
@@ -24,132 +24,202 @@ namespace user {
 
   template <Dimension D>
   class InitFields {
-    public:
-    InitFields(const std::string& file_bx1, 
-               const std::string& file_bx2,
-               const std::pair<real_t, real_t>& extent_x1,
-               const std::pair<real_t, real_t>& extent_x2);
+  public:
 
-    // 插值函数
+    // 修改 interpolate 函数，支持任意维度
     Inline real_t interpolate(const ndfield_t<D, 1>& data, const coord_t<D>& x_Ph) const {
       // 获取插值点的坐标
-      real_t x = x_Ph[0];
-      real_t y = x_Ph[1];
+      real_t xi[D];
+      real_t t[D];
+      size_t idx[D];
 
-      // 计算网格间距
-      real_t dx1 = (extent_x1.second - extent_x1.first) / (nx1 - 1);
-      real_t dx2 = (extent_x2.second - extent_x2.first) / (nx2 - 1);
+      // 计算每个维度的网格间距和索引
+      for (int d = 0; d < D; ++d) {
+        real_t dx = (extent_x[d].second - extent_x[d].first) / (nx[d] - 1);
+        xi[d] = (x_Ph[d] - extent_x[d].first) / dx;
+        idx[d] = static_cast<size_t>(xi[d]);
+        if (idx[d] >= nx[d] - 1) idx[d] = nx[d] - 2;
+        t[d] = xi[d] - idx[d];
+      }
 
-      // 计算网格索引
-      real_t xi = (x - extent_x1.first) / dx1;
-      real_t yi = (y - extent_x2.first) / dx2;
-      size_t i = static_cast<size_t>(xi);
-      size_t j = static_cast<size_t>(yi);
-
-      // 确保索引在有效范围内
-      if (i >= nx1 - 1) i = nx1 - 2;
-      if (j >= nx2 - 1) j = nx2 - 2;
-
-      // 计算权重
-      real_t tx = xi - i;
-      real_t ty = yi - j;
-
-      // 获取周围节点的值
-      real_t f00 = data(i, j);
-      real_t f10 = data(i + 1, j);
-      real_t f01 = data(i, j + 1);
-      real_t f11 = data(i + 1, j + 1);
-
-      // 双线性插值
-      return (1 - tx) * (1 - ty) * f00 + tx * (1 - ty) * f10 + (1 - tx) * ty * f01 + tx * ty * f11;
+      // 进行多维线性插值
+      if constexpr (D == Dim::_1D) {
+        // 一维插值
+        real_t f0 = data(idx[0]);
+        real_t f1 = data(idx[0] + 1);
+        return (1 - t[0]) * f0 + t[0] * f1;
+      } else if constexpr (D == Dim::_2D) {
+        // 二维插值
+        real_t f00 = data(idx[0], idx[1]);
+        real_t f10 = data(idx[0] + 1, idx[1]);
+        real_t f01 = data(idx[0], idx[1] + 1);
+        real_t f11 = data(idx[0] + 1, idx[1] + 1);
+        return (1 - t[0]) * (1 - t[1]) * f00
+             + t[0] * (1 - t[1]) * f10
+             + (1 - t[0]) * t[1] * f01
+             + t[0] * t[1] * f11;
+      } else if constexpr (D == Dim::_3D) {
+        // 三维插值
+        real_t f000 = data(idx[0], idx[1], idx[2]);
+        real_t f100 = data(idx[0] + 1, idx[1], idx[2]);
+        real_t f010 = data(idx[0], idx[1] + 1, idx[2]);
+        real_t f110 = data(idx[0] + 1, idx[1] + 1, idx[2]);
+        real_t f001 = data(idx[0], idx[1], idx[2] + 1);
+        real_t f101 = data(idx[0] + 1, idx[1], idx[2] + 1);
+        real_t f011 = data(idx[0], idx[1] + 1, idx[2] + 1);
+        real_t f111 = data(idx[0] + 1, idx[1] + 1, idx[2] + 1);
+        return ((1 - t[0]) * (1 - t[1]) * (1 - t[2]) * f000
+              + t[0] * (1 - t[1]) * (1 - t[2]) * f100
+              + (1 - t[0]) * t[1] * (1 - t[2]) * f010
+              + t[0] * t[1] * (1 - t[2]) * f110
+              + (1 - t[0]) * (1 - t[1]) * t[2] * f001
+              + t[0] * (1 - t[1]) * t[2] * f101
+              + (1 - t[0]) * t[1] * t[2] * f011
+              + t[0] * t[1] * t[2] * f111);
+      }
     }
 
-    // 磁场分量函数
+    // 根据维度定义磁场分量函数
     Inline auto bx1(const coord_t<D>& x_Ph) const {
       return interpolate(bx1_data, x_Ph);
     }
 
     Inline auto bx2(const coord_t<D>& x_Ph) const {
-      return interpolate(bx2_data, x_Ph);
+      if constexpr (D >= Dim::_2D) {
+        return interpolate(bx2_data, x_Ph);
+      } else {
+        return 0.0;
+      }
+    }
+
+    Inline auto bx3(const coord_t<D>& x_Ph) const {
+      if constexpr (D == Dim::_3D) {
+        return interpolate(bx3_data, x_Ph);
+      } else {
+        return 0.0;
+      }
+    }
+
+    // 修改构造函数，适应不同维度的数据读取
+    InitFields(const std::vector<std::string>& files_bx,
+               const boundaries_t<real_t>* extent_x)
+      : extent_x { extent_x } {
+      // 初始化 nx 和数据容器
+      for (int d = 0; d < D; ++d) {
+        nx[d] = 0;
+      }
+
+      // 读取 bx1 数据
+      std::ifstream input_bx1(files_bx[0]);
+      if (!input_bx1.is_open()) {
+        throw std::runtime_error("无法打开文件：" + files_bx[0]);
+      }
+
+      std::vector<real_t> temp_bx1;
+      std::string line;
+
+      while (std::getline(input_bx1, line)) {
+        std::istringstream iss(line);
+        real_t value;
+        while (iss >> value) {
+          temp_bx1.push_back(value);
+        }
+      }
+      input_bx1.close();
+
+      nx[0] = static_cast<size_t>(std::sqrt(temp_bx1.size()));
+      if constexpr (D >= Dim::_2D) {
+        nx[1] = nx[0];
+      }
+      if constexpr (D == Dim::_3D) {
+        nx[2] = nx[0];
+      }
+
+      bx1_data = ndfield_t<D, 1>("bx1_data", nx[0], nx[1], nx[2]);
+      auto bx1_data_mirror = Kokkos::create_mirror_view(bx1_data);
+      for (size_t i = 0; i < nx[0]; ++i) {
+        for (size_t j = 0; j < nx[1]; ++j) {
+          for (size_t k = 0; k < nx[2]; ++k) {
+            bx1_data_mirror(i, j, k) = temp_bx1[i * nx[1] * nx[2] + j * nx[2] + k];
+          }
+        }
+      }
+      Kokkos::deep_copy(bx1_data, bx1_data_mirror);
+
+      // 根据维度 D，读取相应的 bx 数据
+      if constexpr (D >= Dim::_2D) {
+        // 读取 bx2 数据
+        std::ifstream input_bx2(files_bx[1]);
+        if (!input_bx2.is_open()) {
+          throw std::runtime_error("无法打开文件：" + files_bx[1]);
+        }
+
+        std::vector<real_t> temp_bx2;
+        while (std::getline(input_bx2, line)) {
+          std::istringstream iss(line);
+          real_t value;
+          while (iss >> value) {
+            temp_bx2.push_back(value);
+          }
+        }
+        input_bx2.close();
+
+        if (temp_bx2.size() != temp_bx1.size()) {
+          throw std::runtime_error("文件 " + files_bx[1] + " 的尺寸与 " + files_bx[0] + " 不一致。");
+        }
+
+        bx2_data = ndfield_t<D, 1>("bx2_data", nx[0], nx[1], nx[2]);
+        auto bx2_data_mirror = Kokkos::create_mirror_view(bx2_data);
+        for (size_t i = 0; i < nx[0]; ++i) {
+          for (size_t j = 0; j < nx[1]; ++j) {
+            for (size_t k = 0; k < nx[2]; ++k) {
+              bx2_data_mirror(i, j, k) = temp_bx2[i * nx[1] * nx[2] + j * nx[2] + k];
+            }
+          }
+        }
+        Kokkos::deep_copy(bx2_data, bx2_data_mirror);
+      }
+      if constexpr (D == Dim::_3D) {
+        // 读取 bx3 数据
+        std::ifstream input_bx3(files_bx[2]);
+        if (!input_bx3.is_open()) {
+          throw std::runtime_error("无法打开文件：" + files_bx[2]);
+        }
+
+        std::vector<real_t> temp_bx3;
+        while (std::getline(input_bx3, line)) {
+          std::istringstream iss(line);
+          real_t value;
+          while (iss >> value) {
+            temp_bx3.push_back(value);
+          }
+        }
+        input_bx3.close();
+
+        if (temp_bx3.size() != temp_bx1.size()) {
+          throw std::runtime_error("文件 " + files_bx[2] + " 的尺寸与 " + files_bx[0] + " 不一致。");
+        }
+
+        bx3_data = ndfield_t<D, 1>("bx3_data", nx[0], nx[1], nx[2]);
+        auto bx3_data_mirror = Kokkos::create_mirror_view(bx3_data);
+        for (size_t i = 0; i < nx[0]; ++i) {
+          for (size_t j = 0; j < nx[1]; ++j) {
+            for (size_t k = 0; k < nx[2]; ++k) {
+              bx3_data_mirror(i, j, k) = temp_bx3[i * nx[1] * nx[2] + j * nx[2] + k];
+            }
+          }
+        }
+        Kokkos::deep_copy(bx3_data, bx3_data_mirror);
+      }
     }
 
   private:
-    ndfield_t<D, 1> bx1_data; // 替代 std::vector<std::vector<real_t>>
-    ndfield_t<D, 1> bx2_data; // 替代 std::vector<std::vector<real_t>>
-    size_t nx1;
-    size_t nx2;
-    std::pair<real_t, real_t> extent_x1;
-    std::pair<real_t, real_t> extent_x2;
+    ndfield_t<D, 1> bx1_data;
+    ndfield_t<D, 1> bx2_data;
+    ndfield_t<D, 1> bx3_data;
+    size_t nx[D];
+    boundaries_t<real_t> extent_x[D];
   };
-
-  template <Dimension D>
-  InitFields<D>::InitFields(const std::string& file_bx1, 
-                            const std::string& file_bx2,
-                            const std::pair<real_t, real_t>& extent_x1,
-                            const std::pair<real_t, real_t>& extent_x2)
-    : extent_x1 { extent_x1 },
-      extent_x2 { extent_x2 } {
-    // 读取 bx1 数据并推断 nx1 和 nx2
-    std::ifstream input_bx1(file_bx1);
-    if (!input_bx1.is_open()) {
-      throw std::runtime_error("无法打开文件：" + file_bx1);
-    }
-
-    std::vector<real_t> temp_bx1;
-    std::string line;
-
-    while (std::getline(input_bx1, line)) {
-      std::istringstream iss(line);
-      real_t value;
-      while (iss >> value) {
-        temp_bx1.push_back(value);
-      }
-    }
-    input_bx1.close();
-
-    nx1 = static_cast<size_t>(std::sqrt(temp_bx1.size()));
-    nx2 = nx1;
-
-    static_assert(D == Dim::_2D, "仅支持二维情况");
-
-    bx1_data = ndfield_t<D, 1>("bx1_data", nx1, nx2);
-    auto bx1_data_mirror = Kokkos::create_mirror_view(bx1_data);
-    for (size_t i = 0; i < nx1; ++i) {
-      for (size_t j = 0; j < nx2; ++j) {
-        bx1_data_mirror(i, j) = temp_bx1[i * nx2 + j];
-      }
-    }
-    Kokkos::deep_copy(bx1_data, bx1_data_mirror);
-
-    // 对 bx2 进行类似的操作
-    std::ifstream input_bx2(file_bx2);
-    if (!input_bx2.is_open()) {
-      throw std::runtime_error("无法打开文件：" + file_bx2);
-    }
-
-    std::vector<real_t> temp_bx2;
-    while (std::getline(input_bx2, line)) {
-      std::istringstream iss(line);
-      real_t value;
-      while (iss >> value) {
-        temp_bx2.push_back(value);
-      }
-    }
-    input_bx2.close();
-
-    if (temp_bx2.size() != temp_bx1.size()) {
-      throw std::runtime_error("文件 " + file_bx2 + " 的尺寸与 " + file_bx1 + " 不一致。");
-    }
-
-    bx2_data = ndfield_t<D, 1>("bx2_data", nx1, nx2);
-    auto bx2_data_mirror = Kokkos::create_mirror_view(bx2_data);
-    for (size_t i = 0; i < nx1; ++i) {
-      for (size_t j = 0; j < nx2; ++j) {
-        bx2_data_mirror(i, j) = temp_bx2[i * nx2 + j];
-      }
-    }
-    Kokkos::deep_copy(bx2_data, bx2_data_mirror);
-  }
 
   template <SimEngine::type S, class M>
   struct PGen : public arch::ProblemGenerator<S, M> {
@@ -169,10 +239,10 @@ namespace user {
 
     inline PGen(const SimulationParams& params, const Metadomain<S, M>& global_domain)
       : arch::ProblemGenerator<S, M> { params },
-        init_flds { params.template get<std::string>("setup.file_bx1"),
-                    params.template get<std::string>("setup.file_bx2"),
-                    global_domain.mesh().extent(in::x1),
-                    global_domain.mesh().extent(in::x2) },
+        init_flds { { params.template get<std::string>("setup.file_bx1"),
+                      params.template get<std::string>("setup.file_bx2") },
+                    { global_domain.mesh().extent(in::x1),
+                      global_domain.mesh().extent(in::x2) } },
         temperature { params.template get<real_t>("setup.temperature", 1.0) } {}
 
 
