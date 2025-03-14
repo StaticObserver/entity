ea685344f9fd0a0ff8d71d1ea44291149f28c28a
StaticObserver <cyysouldancer@gmail.com>
2025-03-14 15:58:56 +0800

    remove atomic_add

--- src/kernels/QED_process.hpp
+++ src/kernels/QED_process.hpp
@@ -10,6 +10,8 @@
 
 #include "framework/containers/particles.h"
 
+#include <Kokkos_Core.hpp>
+
 namespace kernel::QED{
     using namespace ntt;
     
@@ -31,72 +33,49 @@ namespace kernel::QED{
         array_t<int*>                 i1_ph;
         array_t<prtldx_t*>            dx1_ph;
 
+        const array_t<size_t*>        N_phs;
+        const array_t<int*>           offsets;
+
         const real_t                  e_min;
-        const real_t                  coeff;
         const real_t                  gamma_emit;
         const real_t                  rho;
         const size_t                  N_max;
         random_number_pool_t          random_pool;
 
-        Kokkos::View<size_t>          num_ph { "num_ph" };
 
 
         public:
-            // CurvatureEmission_kernel(const array_t<real_t*>        ux1_, 
-            //                          const array_t<real_t*>        ux2_,
-            //                          const array_t<real_t*>        ux3_,
-            //                          const array_t<real_t*>        weight_,  
-            //                          const array_t<short*>         tag_,
-            //                          const array_t<int*>           i1_,
-            //                          const array_t<prtldx_t*>      dx1_,
-            //                          array_t<real_t*>              ux1_ph_,
-            //                          array_t<real_t*>              ux2_ph_,
-            //                          array_t<real_t*>              ux3_ph_,
-            //                          array_t<real_t*>              weight_ph_,
-            //                          array_t<short*>               tag_ph_,
-            //                          array_t<real_t**>             pld_ph_,
-            //                          array_t<int*>                 i1_ph_,
-            //                          array_t<prtldx_t*>            dx1_ph_,
-            //                          const real_t                  e_min_,
-            //                          const real_t                  coeff_,
-            //                          const real_t                  gamma_emit_,
-            //                          const real_t                  rho_,
-            //                          const real_t                  npart_ph,
-            //                          const size_t                  N_max,
-            //                          random_number_pool_t&         random_pool_)
-            CurvatureEmission_kernel(Particles<D, C>&    charges,
-                                     Particles<D, C>&    photons,
-                                     const real_t        e_min_,
-                                     const real_t        coeff_,
-                                     const real_t        gamma_emit_,
-                                     const real_t        rho_,
-                                     const size_t        N_max,
-                                     const size_t        npart_ph,
-                                     random_number_pool_t& random_pool_)
-                : ux1 { charges.ux1_ }
-                , ux2 { charges.ux2_ }
-                , ux3 { charges.ux3_ }
-                , weight { charges.weight_ }
-                , tag { charges.tag_ }
-                , i1 { charges.i1_ }
-                , dx1 { charges.dx1_ }
-                , ux1_ph { photons.ux1_ph_ }
-                , ux2_ph { photons.ux2_ph_ }
-                , ux3_ph { photons.ux3_ph_ }
-                , weight_ph { photons.weight_ph_ }
-                , tag_ph { photons.tag_ph_ }
-                , pld_ph { photons.pld_ph_ }
-                , i1_ph { photons.i1_ph_ }
-                , dx1_ph { photons.dx1_ph_ }
+            CurvatureEmission_kernel(Particles<D, C>&          charges,
+                                     Particles<D, C>&          photons,
+                                     const real_t              e_min_,
+                                     const real_t              gamma_emit_,
+                                     const real_t              rho_,
+                                     const size_t              N_max,
+                                     const array_t<size_t*>&   N_phs_,
+                                     const array_t<int*>&      offsets_,
+                                     random_number_pool_t&     random_pool_)
+                : ux1 { charges.ux1 }
+                , ux2 { charges.ux2 }
+                , ux3 { charges.ux3 }
+                , weight { charges.weight }
+                , tag { charges.tag }
+                , i1 { charges.i1 }
+                , dx1 { charges.dx1 }
+                , ux1_ph { photons.ux1 }
+                , ux2_ph { photons.ux2 }
+                , ux3_ph { photons.ux3 }
+                , weight_ph { photons.weight }
+                , tag_ph { photons.tag }
+                , pld_ph { photons.pld }
+                , i1_ph { photons.i1 }
+                , dx1_ph { photons.dx1 }
                 , e_min { e_min_ }
-                , coeff { coeff_ }
                 , gamma_emit { gamma_emit_ }
                 , rho { rho_ }
                 , N_max { N_max }
-                , npart_ph { photons.npart() }
-                , random_pool { random_pool_ } {
-                    Kokkos::deep_copy(num_ph, 0);
-                }
+                , N_phs { N_phs_ }
+                , offsets { offsets_ }
+                , random_pool { random_pool_ } { }
             ~CurvatureEmission_kernel() = default;
 
             Inline auto CDF(real_t zeta_) const -> real_t{
@@ -114,40 +93,88 @@ namespace kernel::QED{
                     }
                     return;
                 }
-                const real_t pp = math::sqrt(ONE + NORM_SQR(ux1(p), ux2(p), ux3(p)));
-                const real_t zeta = e_min * rho * CUBE(gamma_emit / pp);
-                auto N_ph = static_cast<size_t>(coeff * CDF(zeta) / SQR(pp));
-                //if N_ph is less than 1, no emission.
-                if (N_ph < 1){
+                if (offsets(p) < 0){
                     return;
                 }
                 // weight correction
                 auto w_crect = ONE;
+                auto N_ph = N_phs(p);
                 if (N_ph > N_max){
-                    N_ph = N_max;
                     w_crect *= N_ph / N_max;
-                }
-                 
-                size_t offset = Kokkos::atomic_fetch_add(&num_ph(), N_ph);
-                offset += npart_ph;
-
-                if (offset + N_ph > ux1_ph.extent(0)){
-                    raise::KernelError(HERE, "Exceeded maximum number of photons.");
+                    N_ph = N_max;
                 }
             
                 for (short i = 0; i < N_ph; ++i){
                     auto rand_gen = random_pool.get_state();
 
-                    ux1_ph(offset + i) = SIGN(ux1(p)) * ONE;
-                    pld_ph(offset + i, 0) = inverseCDF(CDF(zeta) * Random<real_t>(rand_gen))
+                    ux1_ph(offsets(p) + i) = SIGN(ux1(p)) * ONE;
+                    pld_ph(offsets(p) + i, 0) = inverseCDF(CDF(zeta) * Random<real_t>(rand_gen))
                                              * CUBE(pp / gamma_emit) / rho;
-                    i1_ph(offset + i) = i1(p);
-                    dx1_ph(offset + i) = dx1(p);
-                    weight_ph(offset + i) = w_crect * weight(p);
-                    tag_ph(offset + i) = ParticleTag::alive;
+                    i1_ph(offsets(p) + i) = i1(p);
+                    dx1_ph(offsets(p) + i) = dx1(p);
+                    weight_ph(offsets(p) + i) = w_crect * weight(p);
+                    tag_ph(offsets(p) + i) = ParticleTag::alive;
 
                     random_pool.free_state(rand_gen);
                 }
+            }
+    };
+    
+    template <Dimension D, Coord::type C>
+    class Curvature_Emission_Number{
+        static_assert(D == Dim::_1D, "Curvature emission is only implemented in 1D");
+        static_assert(C == Coord::Cart, "Curvature emission is only implemented in cartesian coordinates");
+
+        const array_t<real_t*>        ux1, ux2, ux3;
+        const array_t<short*>         tag;
+        
+        array_t<size_t*>              N_phs;
+
+        const real_t                  e_min;
+        const real_t                  coeff;
+        const real_t                  gamma_emit;
+        const real_t                  rho;
+
+       
+        Inline auto CDF(real_t zeta_) const -> real_t{
+            return math::exp(-zeta_); 
+        }
+
+        public:
+            Curvature_Emission_Number(const Particles<D, C>& charge,
+                                       const real_t e_min_,
+                                       const real_t coeff_,
+                                       const real_t gamma_emit_,
+                                       const real_t rho_,
+                                       array_t<size_t*>& N_phs_)
+                : ux1 { charge.ux1 }
+                , ux2 { charge.ux2 }
+                , ux3 { charge.ux3 }
+                , tag { charge.tag }
+                , e_min { e_min_ }
+                , coeff { coeff_ }
+                , gamma_emit { gamma_emit_ }
+                , rho { rho_ }
+                , N_phs { N_phs_ }  {
+                    Kokkos::deep_copy(N_phs, 0);
+                    if (N_phs.extent(0) != charge.npart()){
+                        raise::KernelError(HERE, "N_phs array size does not match the number of particles.");
+                    }
+                }
+            ~Curvature_Emission_Number() = default;
+
+            Inline void operator()(index_t p) const{
+                if (tag(p) != ParticleTag::alive){
+                    if (tag(p) != ParticleTag::dead){
+                        raise::KernelError(HERE, "Invalid particle tag in pusher");
+                    }
+                    return;
+                }
+                const real_t pp = math::sqrt(ONE + NORM_SQR(ux1(p), ux2(p), ux3(p)));
+                const real_t zeta = e_min * rho * CUBE(gamma_emit / pp);
+                auto N_ph = static_cast<size_t>(coeff * CDF(zeta) / SQR(pp));
+
+                N_phs(p) = N_ph;
             }
     };
 } // namespace QED
--- src/kernels/tests/QED_process.cpp
+++ src/kernels/tests/QED_process.cpp
@@ -9,6 +9,7 @@
 #include "framework/containers/particles.h"
 
 #include <Kokkos_Core.hpp>
+#include <Kokkos_ScatterView.hpp>
 
 #include <iostream>
 #include <fstream>
@@ -26,6 +27,7 @@ auto main(int argc, char* argv[]) -> int {
     Particles<Dim::_1D, Coord::Cart> electron(1, "e-", 1.0, -1.0, N_e, PrtlPusher::BORIS, false, Cooling::NONE, 0);
     Particles<Dim::_1D, Coord::Cart> photon(2, "photon", 0.0, 0.0, N_ph, PrtlPusher::PHOTON, false, Cooling::NONE, 1);
     
+    const real_t gamma { 1e6 };
 
     auto& ux1 = electron.ux1;
     auto& ux2 = electron.ux2;
@@ -36,7 +38,7 @@ auto main(int argc, char* argv[]) -> int {
     auto& dx1 = electron.dx1;
 
     Kokkos::parallel_for("Init", N_e, KOKKOS_LAMBDA(index_t p) {
-      ux1(p) = 0.0;
+      ux1(p) = math::sqrt(SQR(gamma) - ONE);
       ux2(p) = 0.0;
       ux3(p) = 0.0;
       weight(p) = 1.0;
@@ -55,54 +57,85 @@ auto main(int argc, char* argv[]) -> int {
     
     random_number_pool_t random_pool;
 
-    const real_t gamma { 1e6 };
     const real_t e_ph { CUBE(gamma / gamma_emit) / rho };
+    
+    array_t<size_t*> N_phs("N_phs", electron.npart());
 
-    CurvatureEmission_kernel<Dim::_1D, Coord::Cart> curvature_emission(electron, photon, e_min, coeff, gamma_emit, rho, 100, photons.npart(), random_pool);
+    Curvature_Emission_Number<Dim::_1D, Coord::Cart> curvature_number(electron, 
+                                                                      e_min, 
+                                                                      coeff, 
+                                                                      gamma_emit, 
+                                                                      rho, 
+                                                                      N_phs);
+    
 
-    Kokkos::parallel_for("CurvatureEmission", N_e, curvature_emission);
+    Kokkos::parallel_for("CurvatureEmissionNumber", electron.rangeActiveParticles(), curvature_number);
 
-    auto n_injected = curvature_emission.num_ph();
+    array_t<int*> offsets("offsets", electron.npart());
+    Kokkos::deep_copy(offsets, -1);
+
+
+    CurvatureEmission_kernel<Dim::_1D, Coord::Cart> curvature_emission(electron, 
+                                                                       photon, 
+                                                                       e_min, 
+                                                                       coeff * 1e9, 
+                                                                       gamma_emit, 
+                                                                       rho, 
+                                                                       100, 
+                                                                       photon.npart(), 
+                                                                       random_pool);
+
+    Kokkos::parallel_for("CurvatureEmission", electron.rangeActiveParticles(), curvature_emission);
+
+    auto n_injected = curvature_emission.num_inj();
+    std::cout << "Number of photons injected: " << n_injected << std::endl;
+
     auto total_ph = photon.npart() + n_injected;
-    photons.set_npart(total_ph);
+    photon.set_npart(total_ph);
 
     size_t num_bins { 100 };
     real_t log_min { math::log(e_min / e_ph) };
     real_t log_max { ONE };
     auto dx = (log_max - log_min) / num_bins;
 
-    auto scatter_ebins = Kokkos::Experimental::ScatterView<size_t>("scatter_ebins", num_bins);
-    auto ebins = scatter_ebins.access();
+    auto e_bins = Kokkos::View<size_t*>("e_bins", num_bins);
+    Kokkos::deep_copy(e_bins, 0);
+    auto scatter_ebins = Kokkos::Experimental::create_scatter_view(e_bins);
 
-    Kokkos::parallel_for("Scatter", photon.rangeActiveParticles(), KOKKOS_LAMBDA(index_t p) {
-      if (photon.tag(p) != ParticleTag::alive) {
+    auto& tag_ph = photon.tag;
+    auto& pld_ph = photon.pld;
+    Kokkos::parallel_for("Count ebins", photon.rangeActiveParticles(), KOKKOS_LAMBDA(index_t p) {
+      if (tag_ph(p) != ParticleTag::alive) {
         return;
       }
-      auto log_e = math::log(photon.pld_ph(p, 0) / e_ph);
+      auto log_e = math::log(pld_ph(p, 0) / e_ph);
       auto bin = static_cast<index_t>((log_e - log_min) / dx);
+      auto access = scatter_ebins.access();
       if (bin < 0) {
-        ebins(0) += 1;
+        access(0) += 1;
       }else if (bin >= num_bins) {
-        ebins(num_bins - 1) += 1;
+        access(num_bins - 1) += 1;
       }else {
-        ebins(bin) += 1;
+        access(bin) += 1;
       }
     }); 
-    Kokkos::Experimental::contribute(ebins, scatter_ebins);
+    Kokkos::Experimental::contribute(e_bins, scatter_ebins);
     
-    auto ebins_h = Kokkos::create_mirror_view(ebins);
-    Kokkos::deep_copy(ebins_h, ebins);
+    auto ebins_h = Kokkos::create_mirror_view(e_bins);
+    Kokkos::deep_copy(ebins_h, e_bins);
 
     std::vector<real_t> bin_centers(num_bins);
     for (size_t i = 0; i < num_bins; ++i) {
       bin_centers[i] = math::exp(log_min + (i + 0.5) * dx);
     }
-
-    std::ofstream file("pdf_ph.dat");
-    for (size_t i = 0; i < num_bins; ++i) {
-      file << bin_centers[i] << " " << ebins_h(i) / photon.npart() << std::endl;
+    
+    if (n_injected > 0) {
+      std::ofstream file("pdf_ph.dat");
+      for (size_t i = 0; i < num_bins; ++i) {
+        file << bin_centers[i] << " " << ebins_h(i) / photon.npart() << std::endl;
+      }
+      file.close();
     }
-    file.close();
 
 
   } catch (std::exception& e) {
