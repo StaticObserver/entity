b864ded1bd43aebdf7ab2a0f3af5aeb4d16d2744
StaticObserver <cyysouldancer@gmail.com>
2025-03-19 16:59:57 +0800

    working version for photon decay

--- src/kernels/QED_process.hpp
+++ src/kernels/QED_process.hpp
@@ -34,7 +34,7 @@ namespace kernel::QED{
         array_t<prtldx_t*>            dx1_ph;
 
         const array_t<size_t*>        N_phs;
-        const array_t<int*>           offsets;
+        const array_t<size_t*>           offsets;
 
         const real_t                  e_min;
         const real_t                  gamma_emit;
@@ -52,7 +52,7 @@ namespace kernel::QED{
                                      const real_t              rho_,
                                      const size_t              N_max,
                                      const array_t<size_t*>&   N_phs_,
-                                     const array_t<int*>&      offsets_,
+                                     const array_t<size_t*>&      offsets_,
                                      random_number_pool_t&     random_pool_)
                 : ux1 { charges.ux1 }
                 , ux2 { charges.ux2 }
@@ -119,6 +119,7 @@ namespace kernel::QED{
                     ux1_ph(offsets(p) + i) = SIGN(ux1(p)) * ONE;
                     pld_ph(offsets(p) + i, 0) = inverseCDF(CDF(zeta) * Random<real_t>(rand_gen))
                                              * CUBE(pp / gamma_emit) / rho;
+                    pld_ph(offsets(p) + i, 1) = ZERO;
                     i1_ph(offsets(p) + i) = i1(p);
                     dx1_ph(offsets(p) + i) = dx1(p);
                     weight_ph(offsets(p) + i) = w_crect * weight(p);
@@ -298,8 +299,9 @@ namespace kernel::QED{
                     auto rand_gen = random_pool.get_state();
 
                     ux1_ph(offset + i) = SIGN(ux1(p)) * ONE;
-                    pld_ph(offset + i, 0) = inverseCDF(CDF(zeta) * (Random<real_t>(rand_gen) + 1e-12))
+                    pld_ph(offset + i, 0) = inverseCDF(CDF(zeta) * Random<real_t>(rand_gen))
                                              * CUBE(pp / gamma_emit) / rho;
+                    pld_ph(offset + i, 1) = ZERO;
                     i1_ph(offset + i) = i1(p);
                     dx1_ph(offset + i) = dx1(p);
                     weight_ph(offset + i) = w_crect * weight(p);
@@ -309,6 +311,193 @@ namespace kernel::QED{
                 }
             }
     };
+
+    template <Dimension D, Coord::type C>
+    class PairCreation_kernel{
+        static_assert(D == Dim::_1D, "Pair creation is only implemented in 1D");
+        static_assert(C == Coord::Cart, "Pair creation is only implemented in cartesian coordinates"); 
+
+        const array_t<real_t*>              ux1_ph, ux2_ph, ux3_ph;
+        const array_t<real_t**>             pld_ph;
+        const array_t<short*>               tag_ph;
+        const array_t<int*>                 i1_ph;
+        const array_t<prtldx_t*>            dx1_ph;
+
+        array_t<bool*>                       should_inj;
+
+        const real_t                              coeff1;
+        const real_t                              coeff2;
+        const real_t                              rho0;
+        const real_t                              L;
+
+        const real_t                              dt;
+        const size_t                              n_steps;
+        
+        Inline auto Rho(const real_t x) const -> real_t{
+            return rho0 * (ONE + 0.8 * x / L);
+        }
+    
+
+        Inline auto integrate(const index_t p, const size_t n_steps) const -> real_t{
+            const real_t dx { ux1_ph(p) * dt / n_steps };
+
+            const real_t rho { Rho(static_cast<real_t>(i1_ph(p)) + static_cast<real_t>(dx1_ph(p))) };
+
+            real_t sum { math::sin(pld_ph(p, 2)) * exp(-coeff2 / (math::sin(pld_ph(p, 2)) * pld_ph(p, 0)))
+                         + math::sin(pld_ph(p, 2) + ux1_ph(p) * dt / rho) * exp(-coeff2 / (math::sin(pld_ph(p, 2) + ux1_ph(p) * dt / rho) * pld_ph(p, 0))) };
+            
+            for (size_t i = 1; i < n_steps; i += 2){
+                sum += FOUR * math::sin(pld_ph(p, 2) + i * dx / rho) * exp(-coeff2 / (math::sin(pld_ph(p, 2)+ i * dx / rho) * pld_ph(p, 0)));
+            }
+
+            for (size_t i = 2; i < n_steps; i += 2){
+                sum += TWO * math::sin(pld_ph(p, 2) + i * dx / rho) * exp(-coeff2 / (math::sin(pld_ph(p, 2) + i * dx / rho) * pld_ph(p, 0)));
+            }
+
+            return dx / THREE * sum;
+
+        }
+
+        public:
+            PairCreation_kernel(Particles<D, C>&          photons,
+                                const real_t              coeff1_,
+                                const real_t              coeff2_,
+                                const real_t              rho0_,
+                                const real_t              L_,
+                                const real_t              dt_,
+                                const size_t              n_steps_,
+                                array_t<bool*>&            should_inj_)
+                : ux1_ph { photons.ux1 }
+                , ux2_ph { photons.ux2 }
+                , ux3_ph { photons.ux3 }
+                , pld_ph { photons.pld }
+                , tag_ph { photons.tag }
+                , i1_ph { photons.i1 }
+                , dx1_ph { photons.dx1 }
+                , coeff1 { coeff1_ }
+                , coeff2 { coeff2_ }
+                , rho0 { rho0_ }
+                , L { L_ }
+                , dt { dt_ }
+                , n_steps { n_steps_ }
+                , should_inj { should_inj_ } {}
+            ~PairCreation_kernel() = default;
+
+            Inline void operator()(index_t p) const{
+                if (tag_ph(p) != ParticleTag::alive){
+                    if (tag_ph(p) != ParticleTag::dead){
+                        raise::KernelError(HERE, "Invalid particle tag in pusher");
+                    }
+                    return;
+                }
+                pld_ph(p, 1) += coeff1 * integrate(p, n_steps);
+                pld_ph(p, 2) += dt * ux1_ph(p) / Rho(static_cast<real_t>(i1_ph(p)) + static_cast<real_t>(dx1_ph(p)));
+                if (pld_ph(p, 0) * math::sin(pld_ph(p, 1)) < TWO){
+                    tag_ph(p) = ParticleTag::dead;
+                    return;
+                }
+                if (pld_ph(p, 1) > ONE){
+                    should_inj(p) = true;
+                } 
+            }
+
+    }; // class PairCreation_kernel
+
+    template <Dimension D, Coord::type C>
+    class InjectPairs_kernel{
+        static_assert(D == Dim::_1D, "Pair creation is only implemented in 1D");
+        static_assert(C == Coord::Cart, "Pair creation is only implemented in cartesian coordinates"); 
+        
+        const array_t<real_t*>              ux1_ph, ux2_ph, ux3_ph;
+        const array_t<real_t**>             pld_ph;
+        const array_t<int*>                 i1_ph;
+        const array_t<prtldx_t*>            dx1_ph;
+        const array_t<short*>               tag_ph;
+        const array_t<real_t*>              weight_ph;
+
+        array_t<real_t*>                   ux1_p, ux2_p, ux3_p;
+        array_t<real_t*>                   weight_p;
+        array_t<short*>                    tag_p;
+        array_t<int*>                      i1_p;
+        array_t<prtldx_t*>                 dx1_p;
+
+        array_t<real_t*>                   ux1_e, ux2_e, ux3_e;
+        array_t<real_t*>                   weight_e;
+        array_t<short*>                    tag_e;
+        array_t<int*>                      i1_e;
+        array_t<prtldx_t*>                 dx1_e;
+
+        array_t<bool*>                    should_inj;
+        array_t<size_t*>                  offsets_e;
+        array_t<size_t*>                  offsets_p;
+
+        public:
+
+            InjectPairs_kernel(Particles<D, C>&          photon,
+                               Particles<D, C>&          electron,
+                               Particles<D, C>&          positron,
+                               array_t<bool*>&           should_inj_,
+                               array_t<size_t*>&         offsets_e_,
+                               array_t<size_t*>&         offsets_p_)
+                : ux1_ph { photon.ux1 }
+                , ux2_ph { photon.ux2 }
+                , ux3_ph { photon.ux3 }
+                , pld_ph { photon.pld }
+                , i1_ph { photon.i1 }
+                , dx1_ph { photon.dx1 }
+                , tag_ph { photon.tag }
+                , weight_ph { photon.weight }
+                , ux1_p { positron.ux1 }
+                , ux2_p { positron.ux2 }
+                , ux3_p { positron.ux3 }
+                , i1_p { positron.i1 }
+                , dx1_p { positron.dx1 }
+                , tag_p { positron.tag }
+                , weight_p { positron.weight }
+                , ux1_e { electron.ux1 }
+                , ux2_e { electron.ux2 }
+                , ux3_e { electron.ux3 }
+                , i1_e { electron.i1 }
+                , dx1_e { electron.dx1 }
+                , tag_e { electron.tag }
+                , weight_e { electron.weight }
+                , should_inj { should_inj_ }
+                , offsets_e { offsets_e_ }
+                , offsets_p { offsets_p_ } {}
+
+            ~InjectPairs_kernel() = default;
+
+            Inline void operator()(index_t p) const{
+                if (!should_inj(p)){
+                    return;
+                }
+                const real_t u = math::abs(math::cos(pld_ph(p, 2))) * math::cos(pld_ph(p, 2))
+                                  * math::sqrt(SQR(pld_ph(p, 0)) - FOUR) 
+                                  / math::sqrt(SQR(math::sin(pld_ph(p, 2) * pld_ph(p, 0))) 
+                                      + FOUR * math::cos(pld_ph(p, 2)));
+
+                ux1_p(offsets_p(p)) = u;
+                ux2_p(offsets_p(p)) = ZERO;
+                ux3_p(offsets_p(p)) = ZERO;
+                i1_p(offsets_p(p)) = i1_ph(p);
+                dx1_p(offsets_p(p)) = dx1_ph(p);
+                weight_p(offsets_p(p)) = weight_ph(p);
+                tag_p(offsets_p(p)) = ParticleTag::alive;
+
+                ux1_e(offsets_e(p)) = u;
+                ux2_e(offsets_e(p)) = ZERO;
+                ux3_e(offsets_e(p)) = ZERO;
+                i1_e(offsets_e(p)) = i1_ph(p);
+                dx1_e(offsets_e(p)) = dx1_ph(p);
+                weight_e(offsets_e(p)) = weight_ph(p);
+                tag_e(offsets_e(p)) = ParticleTag::alive;
+
+                tag_ph(p) = ParticleTag::dead;
+            }
+                
+    }; // class InjectPairs_kernel
+
+
 } // namespace QED
 
 
--- src/kernels/tests/QED_process.cpp
+++ src/kernels/tests/QED_process.cpp
@@ -23,10 +23,10 @@ auto main(int argc, char* argv[]) -> int {
   Kokkos::initialize(argc, argv);
 
   try {
-    size_t N_e = 1e4;
-    size_t N_ph = 1e7;
+    size_t N_e = 1e6;
+    size_t N_ph = 1e6;
     Particles<Dim::_1D, Coord::Cart> electron(1, "e-", 1.0, -1.0, N_e, PrtlPusher::BORIS, false, Cooling::NONE, 0);
-    Particles<Dim::_1D, Coord::Cart> photon(2, "photon", 0.0, 0.0, N_ph, PrtlPusher::PHOTON, false, Cooling::NONE, 1);
+    Particles<Dim::_1D, Coord::Cart> photon(2, "photon", 0.0, 0.0, N_ph, PrtlPusher::PHOTON, false, Cooling::NONE, 3);
     
     const real_t gamma { 1e6 };
 
@@ -38,16 +38,16 @@ auto main(int argc, char* argv[]) -> int {
     auto& i1 = electron.i1;
     auto& dx1 = electron.dx1;
 
-    electron.set_npart(N_e);
+    electron.set_npart(1e3);
 
     Kokkos::parallel_for("Init", electron.rangeActiveParticles(), KOKKOS_LAMBDA(index_t p) {
       ux1(p) = math::sqrt(SQR(gamma) - ONE);
-      ux2(p) = 0.0;
-      ux3(p) = 0.0;
-      weight(p) = 1.0;
+      ux2(p) = ZERO;
+      ux3(p) = ZERO;
+      weight(p) = ONE;
       tag(p) = ParticleTag::alive;
       i1(p) = 0;
-      dx1(p) = 0;
+      dx1(p) = ZERO;
     });
 
     const real_t e_min { 2.0 };
@@ -79,12 +79,12 @@ auto main(int argc, char* argv[]) -> int {
     // Kokkos::fence();
     // std::cout << "End curvature emission number." << std::endl;
 
-    array_t<int*> offsets("offsets", electron.npart());
+    array_t<size_t*> offsets("offsets", electron.npart());
     Kokkos::deep_copy(offsets, 0);
 
-    int n_injected { 0 };
+    size_t n_injected { 0 };
     // std::cout << "Begin scan." << std::endl;
-    Kokkos::parallel_scan("Scan", N_phs.extent(0), KOKKOS_LAMBDA(index_t p, int& update, const bool final) {
+    Kokkos::parallel_scan("Scan", N_phs.extent(0), KOKKOS_LAMBDA(index_t p, size_t& update, const bool final) {
       if (final) {
         offsets(p) = update;
       }
@@ -149,12 +149,13 @@ auto main(int argc, char* argv[]) -> int {
     real_t max { 20.0 };
     auto dx = (max - min) / num_bins;
 
-    auto e_bins = Kokkos::View<size_t*>("e_bins", num_bins);
+    auto e_bins = Kokkos::View<real_t*>("e_bins", num_bins);
     Kokkos::deep_copy(e_bins, 0);
     auto scatter_ebins = Kokkos::Experimental::create_scatter_view(e_bins);
 
     auto& tag_ph = photon.tag;
     auto& pld_ph = photon.pld;
+    auto& weight_ph = photon.weight;
     Kokkos::parallel_for("Count ebins", photon.rangeActiveParticles(), KOKKOS_LAMBDA(index_t p) {
       if (tag_ph(p) != ParticleTag::alive) {
         return;
@@ -163,11 +164,11 @@ auto main(int argc, char* argv[]) -> int {
       auto bin = static_cast<index_t>((e - min) / dx);
       auto access = scatter_ebins.access();
       if (bin < 0) {
-        access(0) += 1;
+        access(0) += weight_ph(p);
       }else if (bin >= num_bins) {
-        access(num_bins - 1) += 1;
+        access(num_bins - 1) += weight_ph(p);
       }else {
-        access(bin) += 1;
+        access(bin) += weight_ph(p);
       }
     }); 
     Kokkos::Experimental::contribute(e_bins, scatter_ebins);
@@ -186,8 +187,155 @@ auto main(int argc, char* argv[]) -> int {
         file << bin_centers[i] << " " << ebins_h(i) << std::endl;
       }
       file.close();
-
     }
+
+    std::cout << "PairCreation" << std::endl;
+    auto start_pair = std::chrono::high_resolution_clock::now();
+
+    array_t<bool*>    should_inj { "should_inj", photon.npart() };
+    Particles<Dim::_1D, Coord::Cart> positron(1, "e+", 1.0, 1.0, N_e, PrtlPusher::BORIS, false, Cooling::NONE, 0);
+
+    PairCreation_kernel<Dim::_1D, Coord::Cart> pair_creation(photon, 
+                                                             1.0,
+                                                             1.0,
+                                                             1.0,
+                                                             1.0,
+                                                             3.0,
+                                                             10,
+                                                             should_inj);
+    Kokkos::parallel_for("PairCreation", photon.rangeActiveParticles(), pair_creation);
+
+    // auto pld_ph_h = Kokkos::create_mirror_view(photon.pld);
+    // auto tag_ph_h = Kokkos::create_mirror_view(photon.tag);
+    // Kokkos::deep_copy(pld_ph_h, photon.pld);
+    // Kokkos::deep_copy(tag_ph_h, photon.tag);
+    // for (size_t i = 0; i < photon.npart(); ++i) {
+    //   if (tag_ph_h(i) == ParticleTag::alive) {
+    //     std::cout << "Probability: " << pld_ph_h(i, 1) << std::endl;
+    //   }
+    // }
+
+    array_t<size_t*> offsets_e { "offsets_e", photon.npart() };
+    array_t<size_t*> offsets_p { "offsets_p", photon.npart() };
+    Kokkos::deep_copy(offsets_e, electron.npart());
+    Kokkos::deep_copy(offsets_p, positron.npart());
+
+    size_t n_injected_e { 0 };
+
+    Kokkos::parallel_scan("Scan_e", should_inj.extent(0), KOKKOS_LAMBDA(index_t p, size_t& update, const bool final) {
+      if (final) {
+        offsets_e(p) += update;
+        offsets_p(p) += update;
+      }
+      update += static_cast<size_t>(should_inj(p));
+    }, n_injected_e);
+
+
+    std::cout << "Number of pairs injected: " << n_injected_e << std::endl;
+
+    electron.set_npart(electron.npart() + n_injected_e);
+    positron.set_npart(positron.npart() + n_injected_e);
+
+    InjectPairs_kernel<Dim::_1D, Coord::Cart> inject_pairs(photon, 
+                                                            electron, 
+                                                            positron, 
+                                                            should_inj, 
+                                                            offsets_e, 
+                                                            offsets_p);
+    Kokkos::parallel_for("InjectPairs", photon.rangeActiveParticles(), inject_pairs);
+
+    Kokkos::fence();
+
+    auto end_pair = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_pair = end_pair - start_pair;
+    std::cout << "Time: " << elapsed_pair.count() << " s" << std::endl;
+
+
+    const real_t min_e { ZERO };
+    const real_t max_e { 1e6 };
+    const real_t dx_e = (max_e - min_e) / num_bins;
+    std::vector<real_t> bin_centers_e(num_bins);
+    for (size_t i = 0; i < num_bins; ++i) {
+      bin_centers_e[i] = min_e + (i + 0.5) * dx_e;
+    }
+    
+
+    array_t<real_t*> e_bins_e { "e_bins_e", num_bins };
+    Kokkos::deep_copy(e_bins_e, 0);
+    auto scatter_ebins_e = Kokkos::Experimental::create_scatter_view(e_bins_e);
+
+    auto& tag_e = electron.tag;
+    auto& ux1_e = electron.ux1;
+    auto& ux2_e = electron.ux2;
+    auto& ux3_e = electron.ux3;
+    auto& weight_e = electron.weight;
+    Kokkos::parallel_for("Count ebins_e", electron.rangeActiveParticles(), KOKKOS_LAMBDA(index_t p) {
+      if (tag_e(p) != ParticleTag::alive) {
+        return;
+      }
+      auto e = math::sqrt(ONE + NORM_SQR(ux1_e(p), ux2_e(p), ux3_e(p))) - ONE;
+      auto bin = static_cast<index_t>((e - min_e) / dx_e);
+      // printf("bin: %lu\n", bin);
+      auto access = scatter_ebins_e.access();
+      if (bin < 0) {
+        access(0) += weight_e(p);
+      }else if (bin >= num_bins) {
+        access(num_bins - 1) += weight_e(p);
+      }else {
+        access(bin) += weight_e(p);
+      }
+    });
+    Kokkos::Experimental::contribute(e_bins_e, scatter_ebins_e);
+
+    auto ebins_e_h = Kokkos::create_mirror_view(e_bins_e);
+    Kokkos::deep_copy(ebins_e_h, e_bins_e);
+
+    if (n_injected_e > 0) {
+      std::ofstream file("pdf_e.dat");
+      for (size_t i = 0; i < num_bins; ++i) {
+        file << bin_centers_e[i] << " " << ebins_e_h(i) << std::endl;
+      }
+      file.close();
+    }
+
+    array_t<real_t*> e_bins_p { "e_bins_p", num_bins };
+    Kokkos::deep_copy(e_bins_p, 0);
+    auto scatter_ebins_p = Kokkos::Experimental::create_scatter_view(e_bins_p);
+
+    auto& tag_p = positron.tag;
+    auto& ux1_p = positron.ux1;
+    auto& ux2_p = positron.ux2;
+    auto& ux3_p = positron.ux3;
+    auto& weight_p = positron.weight;
+    Kokkos::parallel_for("Count ebins_p", positron.rangeActiveParticles(), KOKKOS_LAMBDA(index_t p) {
+      if (tag_p(p) != ParticleTag::alive) {
+        return;
+      }
+      auto e = math::sqrt(ONE + NORM_SQR(ux1_p(p), ux2_p(p), ux3_p(p))) - ONE;
+      auto bin = static_cast<index_t>((e - min_e) / dx_e);
+      // printf("bin: %lu\n", bin);
+      auto access = scatter_ebins_p.access();
+      if (bin < 0) {
+        access(0) += weight_p(p);
+      }else if (bin >= num_bins) {
+        access(num_bins - 1) += weight_p(p);
+      }else {
+        access(bin) += weight_p(p);
+      }
+    });
+    Kokkos::Experimental::contribute(e_bins_p, scatter_ebins_p);
+
+    auto ebins_p_h = Kokkos::create_mirror_view(e_bins_p);
+    Kokkos::deep_copy(ebins_p_h, e_bins_p);
+
+    if (n_injected_e > 0) {
+      std::ofstream file("pdf_p.dat");
+      for (size_t i = 0; i < num_bins; ++i) {
+        file << bin_centers_e[i] << " " << ebins_p_h(i) << std::endl;
+      }
+      file.close();
+    }
+
 
 
   } catch (std::exception& e) {
