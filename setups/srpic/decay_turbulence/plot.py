64abe83da743660f4688b5f47699df8ae43f78d6
StaticObserver <cyysouldancer@gmail.com>
2025-03-26 15:31:40 +0800

    change plot.py in decay_turb

--- setups/srpic/decay_turbulence/plot.py
+++ setups/srpic/decay_turbulence/plot.py
@@ -1,35 +1,37 @@
-import sys
-sys.path.append("/work/home/acdnhs4d3w/Entity-dev/decay_turbulence/")
-import visualize as vis
-import nt2.read as nt2r
-import nt2.export as exp
+import nt2
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 
-data = nt2r.Data("turb-decay.h5")
+data = nt2.Data(path="turbulence")
 
-max_t = data.t.max().item()
-steps = np.linspace(0, max_t, len(data.t))
+#data.fields.inspect.plot(name="inspect", only_fields=["N", "Jz"])
 
+def plot_spectra(t, data):
+    frame = data.sel({'t':t}, method="nearest")
+    plt.figure(figsize=(6, 3))
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.plot(frame.e, frame.n_1 + frame.n_2, c="r")
+    plt.ylim(10, 1e8)
+    plt.xlabel(r"$\gamma - 1$")
+    plt.xlim(frame.e.min(), 1e3)
+
+def plot_func(t, fld):
+    vmin = 0.0
+    vmax = 3.0
+    fig, ax = plt.subplots(figsize=(10, 8), dpi=150)
+    colormap = 'inferno'
+    #colormap ='jet'
+    fld.sel({'t':t}, method='nearest').plot(ax=ax, norm=mpl.colors.Normalize(vmin, vmax), cmap=colormap)
 
 def main():
-    field = 'Bx'
-    means = ['Prtl_Energy', 'Total_Energy', 'B2'] 
-    d = 1.0
-    rho = 0.2
-    num_cpus = 4
-    #for mean in means:
-     # vis.plot_mean(data, mean, d, rho) 
-    #vis.plot_mean(data, 'Total_Energy', d, rho)
-    #vis.plot_attr(steps, data, field, 'norm', 0.0, 3.0, num_cpus=num_cpus)
-    #exp.makeMovie(input=field+"/frames/", overwrite=True, output=field + "/" + field + ".mp4", number=5)
+    num_cpus = 32
+    times = np.linspace(0, 1000, 200)
+    #sp = data.spectra
     
-    plot_data = getattr(data, field)
-    exp.makeFrames(lambda t, data: data.sel({'t':t}, method="nearest").spectrum.plot(),
-                   steps,
-                   field + "_spec/frames",
-                   plot_data,
-                   num_cpus=num_cpus)
-    #print(getattr(data,'Bx').sel({'t':1.0}, method="nearest"))
-
+    #nt2.export.makeFrames(plot_spectra, times, 'spectra', sp, num_cpus=num_cpus)
+    #nt2.export.makeFrames(plot_func, times, 'N', data.fields.N, num_cpus=num_cpus)
+    print(data)
 if __name__ == '__main__':
     main()
